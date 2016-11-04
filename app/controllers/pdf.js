49a57d1721d028fdb2a589e73bf08853664b51ac
Anton Konyushevskiy <a@konyushevskiy.com>
2016-11-04 16:01:05 +0500

    mispell fix

--- app/controllers/pdf.js
+++ app/controllers/pdf.js
@@ -21,7 +21,7 @@ exports.render = function (req, res) {
 		};
 
 		console.log(path.resolve(`${__dirname}/../views/pdf.ejs`));
-		console.log(path.resolve(`${__dirname}/../../files/match.pdf`);
+		console.log(path.resolve(`${__dirname}/../../files/match.pdf`));
 
 		/*res.render(`${__dirname}/../views/pdf.ejs`, match);*/
 		ejs.renderFile(path.resolve(`${__dirname}/../views/pdf.ejs`), match, {}, function(err, str){
